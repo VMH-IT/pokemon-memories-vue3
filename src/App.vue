63559e28e1eb53b3b53c4b8cd3bf1c996e2f0736
VMH-IT <vmh.it170701@gmail.com>
2023-08-04 05:34:17 +0700

    build game

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart($event)" />
+  <interact-screen v-if="statusMatch === 'match'" />
+</template>
+
+<script>
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+// import FooterScreen from "./components/FooterScreen.vue";
+// import ResultScreen from "./components/FooterScreen.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      statusMatch: "default",
+    };
+  },
+
+  components: {
+    MainScreen,
+    // FooterScreen,
+    InteractScreen,
+    // ResultScreen
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("running ...", config);
+      this.statusMatch = "match"
+    }
+  }
+};
+</script>
---
+++ src/components/InteractScreen.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="screen">
+    <h1>test</h1>
+    <card-flip />
+  </div>
+</template>
+<script>
+import CardFlip from "./SingleCard.vue"
+export default {
+  components: {
+    CardFlip
+  },
+}
+</script>
